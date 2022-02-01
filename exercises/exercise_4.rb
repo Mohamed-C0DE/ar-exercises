9facf6f41c7abc9dba91ed206b557532f5035d24
Mohamed Ali <12maali27@gmail.com>
2022-02-01 19:49:49 +0000

    added new stores and looped through each store in the Stores database to see which had men and which had womens clothing and logged each store if they contained either and their annual revenue

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,20 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+surrey = Store.create(name: 'Surrey' ,annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+whistler = Store.create(name: 'Whistler', annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+yaletown = Store.create(name: 'Yaletown', annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+@men_stores = Store.where(mens_apparel: true)
+
+@men_stores.each do |store|
+    p "#{store.name} carries mens clothing and has an annual revenue of $#{store.annual_revenue}"
+end
+
+
+@women_stores = Store.where(womens_apparel: true)
+
+@women_stores.each do |store|
+    p "#{store.name} carries womens clothing and has an annual revenue of $#{store.annual_revenue}"
+end
